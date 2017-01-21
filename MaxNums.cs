3f21535de00d28a3e25f48289d44a5228f8020e6
adeto <adeto@users.noreply.github.com>
2017-01-21 13:38:46 +0200

    Maximum Numbers

---
+++ MaxNums.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05.MaxNumber
+{
+    class MaxNums
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var max = -10000000000000;
+
+            for (var i = 1; i <= n; i++)
+            {
+                var num = int.Parse(Console.ReadLine());
+                if (num > max)
+                    max = num;
+                {
+                    Console.WriteLine("" + max);
+                }
+              
+            }
+        }
+    }
+}
