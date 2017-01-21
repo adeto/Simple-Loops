5d9b6f9143723031b3c1fac682a96b0a479472e9
adeto <adeto@users.noreply.github.com>
2017-01-21 13:37:21 +0200

    Numbers from 1 to 100

---
+++ Numbers_1_To_100.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01.Numbers_form_1_to_100
+{
+    class Numbers_1_To_100
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
