00f777e2b9f316868aebe75d50fcd13b1a235191
adeto <adeto@users.noreply.github.com>
2017-01-21 13:39:11 +0200

    Minimum Numbers

---
+++ MinNums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06.MinNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var min = 10000000000000;
+            for (int i = 1; i <= n; i++)
+            {
+                var num = int.Parse(Console.ReadLine());
+
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+
+                Console.WriteLine(min);
+                
+
+            
+        }
+    }
+}
