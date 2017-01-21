29d8230fdddc8309bcb954ff8453c1051c6182e2
adeto <adeto@users.noreply.github.com>
2017-01-21 13:37:42 +0200

    Numbers ending 7

---
+++ Num_End_7.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02.NumbersEnding7
+{
+    class Num_End_7
+    {
+        static void Main()
+        {
+            for (var i = 0; i <= 1000; i++)
+            {
+                if (i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
