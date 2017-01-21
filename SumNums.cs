ea7c6a6308eec4f7a9fba7ca7841aa848f640eca
adeto <adeto@users.noreply.github.com>
2017-01-21 13:38:20 +0200

    Sum Numbers

---
+++ SumNums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04.SumNumbers
+{
+    class SumNums
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var num = int.Parse(Console.ReadLine());
+                sum = num + sum;
+
+            }
+            Console.WriteLine(""+ sum);
+        }
+    }
+}
