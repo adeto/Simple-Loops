82c0355a460fb6b84b12f35acf23ef3ce441841b
adeto <adeto@users.noreply.github.com>
2017-01-21 13:39:43 +0200

    Left and Right Sum

---
+++ Left and Right Sum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _07.Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var left = 0;
+            for (int i = 0; i < n; i++)
+            {
+                left = left + int.Parse(Console.ReadLine());
+            }
+
+            var right = 0;
+            for (int i = 0; i < n; i++)
+            {
+                right = right + int.Parse(Console.ReadLine());
+            }
+
+            if (left == right)
+            {
+                Console.WriteLine("Yes, sum =" + left);
+            }
+            else 
+            {
+                Console.WriteLine("No, diff =" + Math.Abs(right - left));
+            }
+
+        }
+    }
+}
