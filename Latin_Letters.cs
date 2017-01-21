3c5cea22d667e59674b8c8227a11affad884b07d
adeto <adeto@users.noreply.github.com>
2017-01-21 13:38:01 +0200

    Latin Alphabets

---
+++ Latin_Letters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03.LatinAlphabet
+{
+    class Latin_Letters
+    {
+        static void Main()
+        {
+            for (var letter = 'a' ; letter <= 'z'; letter++)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
